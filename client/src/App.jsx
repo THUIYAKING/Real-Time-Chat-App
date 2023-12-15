179fbc032b5f6a180eaba4c19e6fdc4de8cf0ed5
THUIYA <Vinthuiya@gmail.com>
2023-12-16 00:26:08 +0300

    css changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import "./App.css";
+import './App.css'
 
-import AuthPage from "./AuthPage";
-import ChatsPage from "./ChatsPage";
+import AuthPage from './AuthPage'
+import ChatsPage from './ChatsPage'
 
 function App() {
-  const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState(undefined)
 
   if (!user) {
-    return <AuthPage onAuth={(user) => setUser(user)} />;
+    return <AuthPage onAuth={user => setUser(user)} />
   } else {
-    return <ChatsPage user={user} />;
+    return <ChatsPage user={user} />
   }
 }
 
-export default App;
+export default App
